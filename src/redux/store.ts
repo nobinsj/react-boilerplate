50779f5cab2bd1e5d7891e80d5798f8c10b202f5
“[fig <your.email@example.com>
2025-01-23 12:03:10 +0530

    setup store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { thunk } from "redux-thunk";
+import { Action, configureStore } from "@reduxjs/toolkit";
+import { thunk, ThunkAction } from "redux-thunk";
 import { rootReducer } from "./rootReducer";
 
 export const store = configureStore({
@@ -9,3 +9,9 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
